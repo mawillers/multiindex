37b053c45669e7d0a2835a81f384c558c7dd2db4
Martin Willers <M.Willers@gmx.net>
2017-03-11 11:29:09 +0100

    Add interface Index<V>

---
+++ src/main/java/com/github/mawillers/multiindex/Index.java
@@ -0,0 +1,39 @@
+package com.github.mawillers.multiindex;
+
+/**
+ * An index into the MultiIndexContainer.
+ *
+ * @param <V> the type of elements in this index
+ */
+public interface Index<V>
+{
+    /**
+     * Adds the specified value to the container.
+     * <p>
+     * An index might place restrictions on what values can be added to it. In particular, certain indexes have uniqueness constrains that prevent certain
+     * values from being added. If any of the indexes rejects this value, this method returns false.
+     *
+     * @param value the value to add
+     * @return true if value has been added to the container
+     */
+    public boolean add(V value);
+
+    /**
+     * Removes all of the elements from the container.
+     */
+    public void clear();
+
+    /**
+     * Returns true if the container contains no elements.
+     *
+     * @return true if the container contains no elements
+     */
+    public boolean isEmpty();
+
+    /**
+     * Returns the number of elements in the container.
+     *
+     * @return the number of elements in the container
+     */
+    public int size();
+}
