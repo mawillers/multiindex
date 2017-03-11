05b2462c06444d976b797079d61d76b65e7707d4
Martin Willers <M.Willers@gmx.net>
2017-03-11 10:28:39 +0100

    Add skeleton for new class MultiIndexContainer

---
+++ src/main/java/com/github/mawillers/multiindex/MultiIndexContainer.java
@@ -0,0 +1,12 @@
+package com.github.mawillers.multiindex;
+
+/**
+ * A container class with dynamic indexes.
+ * <p>
+ * This container stores values of a given type, and allows retrieval of these values by an arbitrary number of indexes.
+ *
+ * @param <V> the type that this Container contains
+ */
+public final class MultiIndexContainer<V>
+{
+}
